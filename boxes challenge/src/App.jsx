b4eac13e906afcf525dd1b6e5e3275c362d41f62
bruyns <brandon.bruyns7@gmail.com>
2023-07-20 13:58:13 +0200

    setting box background color

--- boxes challenge/src/App.jsx
+++ boxes challenge/src/App.jsx
@@ -1,18 +1,30 @@
 import React from "react"
 import boxes from "./boxes.jsx"
 
-export default function App() {
-    /**
-     * Challenge part 1:
-     * 1. Initialize state with the default value of the
-     *    array pulled in from boxes.js
-     * 2. Map over that state array and display each one
-     *    as an empty square (black border, transparent bg color)
-     *    (Don't worry about using the "on" property yet)
-     */
+export default function App(props) {
+    const [squares, setSquares] = React.useState(boxes)
+
+    /* using round braces instead of curly braces in a aroow function creats an implicit return
+       does not have to set a return as it automatically returns everything within the round braces 
+       whenever the function runs
+    */
+// in style the first set of curly bois are to enter into javascipt within react and the 2nd set of curly boys
+// sets the acual JS style method
+    props.darkMode( boxes ? "#222222" : "#cccccc")
+    const styles = {
+      backgroundColor: {darkMode},
+    }
+
+    const squareElements = squares.map(square => (
+      <div style={styles} className="box" key={square.id}>
+        
+      </div>
+    ))
+
+
     return (
         <main>
-            <h1>Boxes will go here</h1>
+            {squareElements}
         </main>
     )
 }
