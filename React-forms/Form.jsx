86e43889c9dbe56ca2621d62a490a73759f213cc
bruyns <brandon.bruyns7@gmail.com>
2023-07-24 17:17:21 +0200

    finished all forms content

--- React-forms/Form.jsx
+++ React-forms/Form.jsx
@@ -26,23 +26,30 @@ export default function Form() {
         emailAdress: "", 
         associateForm: "", 
         comments: "",
-        isFriendly: true
+        isFriendly: true,
+        employment: "",
+        favColor: ""
         }
        )
        console.log(formData)
        
         function handleChange(event) {
-            const {name, value} = event.target
+            const {name, value, type, checked} = event.target
             setFormData(prevFormData => {
                 return {
                     ...prevFormData,
-                    [name]: value
+                    [name]: type === "checkbox" ? checked : value
                 }
             })
         }
 
+        function handleSubmit(event) {
+            event.preventDefault()
+            console.log(formData)
+        }
+
         return (
-            <form className="Form">
+            <form onSubmit={handleSubmit} className="Form">
                 <input
                     type="text"
                     placeholder="First Name"
@@ -78,6 +85,7 @@ export default function Form() {
                     name={"comments"} 
                 />
                 <input
+                    name="isFriendly"
                     type="checkbox"
                     id="isFriendly"
                     checked={formData.isFriendly}
@@ -85,6 +93,61 @@ export default function Form() {
                 />
                 <label htmlFor="isFriendly">Are you friendly?</label>
                 <br />
+                <br />
+
+                <fieldset>
+                    <legend>Current employment status</legend>
+                    <input
+                        name="employment"
+                        type="radio"
+                        id="unemployed"
+                        value="unemployed"
+                        checked={formData.employment === "unemployed"}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="unemployed">Unemployed</label>
+                    <br />
+                    <input
+                        name="employment"
+                        type="radio"
+                        id="part-time"
+                        value="part-time"
+                        checked={formData.employment === "part-time"}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="part-time">Part-time</label>
+                    <br />
+                    <input
+                        name="employment"
+                        type="radio"
+                        id="full-time"
+                        value="full-time"
+                        checked={formData.employment === "full-time"}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="full-time">Full-time</label>
+                    <br />
+                </fieldset>
+                    <br />
+
+                <label htmlFor="favColor">What is your favourite color?</label>
+                <br />
+                <select 
+                    id="favColor"
+                    value={formData.favColor}
+                    onChange={handleChange}
+                    name="favColor"
+                >
+                    <option value="">-- Choose --</option>
+                    <option value="red">Red</option>
+                    <option value="orange">Orange</option>
+                    <option value="yellow">Yellow</option>
+                    <option value="green">Green</option>
+                    <option value="blue">Blue</option>
+                    <option value="indigo">Indigo</option>
+                    <option value="violet">Violet</option>
+                </select>
+                <button>submit</button>
             </form>
         )
 }
